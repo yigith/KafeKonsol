9b49bdad89064482af07453852f53be6ff668468
yigith <yigith1@gmail.com>
2023-05-23 10:12:12 +0300

    data classes

---
+++ KafeKonsol.Data/SiparisDetay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafeKonsol.Data
+{
+    public class SiparisDetay
+    {
+        public string UrunAd { get; set; } = null!;
+
+        public decimal BirimFiyat { get; set; }
+
+        public int Adet { get; set; }
+
+        public string TutarTL { get { return Tutar().ToString("c2"); } }
+
+        public decimal Tutar()
+        {
+            return Adet * BirimFiyat;
+        }
+    }
+}
